7be5dc322427b11d4ce4fc746d0a7634130f3fe8
Emmy3131 <devinechild30032@gmail.com>
2024-09-22 01:18:46 +0100

    mobile button activated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+var menuButton = document.getElementById("mobile-nav-icon");
+
+menuButton.addEventListener('click',function(){
+    var navMenu = document.getElementById("nav-menu");
+   navMenu.style.left=='-200px'? navMenu.style.left='16px': navMenu.style.left="-200px"
+})
